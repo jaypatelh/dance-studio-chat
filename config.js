86ac015f5f5bd01135f4c29f4b83bca06d10d32b
Jay Patel <jaypatelh@gmail.com>
2025-09-18 11:24:35 -0700

    fixed chat input, messages go behind. like chatgpt.com

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration - placeholders will be replaced during build process - Updated API key
 const config = {
-    googleApiKey: '{{GOOGLE_API_KEY}}',
-    openRouterApiKey: '{{OPENROUTER_API_KEY}}',
+    googleApiKey: 'AIzaSyC2Ru0Ov7RaNBwXwF0nbGvEhQsurkbNFnM',
+    openRouterApiKey: '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',
     ownerPhoneNumber: '+16509954591'
 };
 
